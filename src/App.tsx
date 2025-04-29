1a9ef36f32fe8f573045240c85660b82711d5870
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-04-29 16:12:03 -0400

    Get sidebar rendering below the top bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { BigList } from "./BigList";
 
-// This lays out the sidebar to the left of the top bar and the main content.
 export const App = () => {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="flex flex-col flex-grow">
-        <div className="p-3 bg-blue-300">17Reviews</div>
-        <main className="p-3 bg-green-300 ">
-          <SidebarTrigger />
-          <BigList expanded />
-        </main>
-      </div>
-    </SidebarProvider>
-  );
-};
-
-// This lays out the sidebar with an inset
-export const AppWithInset = () => {
-  return (
-    <SidebarProvider>
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <div className="flex flex-col flex-grow">
-          <div className="p-3 bg-blue-300">17Reviews</div>
-          <main className="p-3 bg-green-300 ">
+    // TODO: test this on mobile - is dvh and dvw correct? or should we use h-screen/w-screen (which use vh/vw)
+    <div className="grid grid-cols-[1fr] grid-rows-[auto_1fr] h-dvh w-dvw overflow-hidden">
+      <div className="p-3 bg-blue-300">17Reviews</div>
+      <div className="bg-yellow-100 overflow-auto">
+        <SidebarProvider className="relative h-full min-h-auto">
+          <AppSidebar className="absolute h-full" />
+          <main className="p-3 bg-green-300 w-full overflow-y-auto">
             <SidebarTrigger />
             <BigList expanded />
           </main>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+        </SidebarProvider>
+      </div>
+    </div>
   );
 };
