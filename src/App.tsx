566cad5334694639b6361b65c9a0baa74cd8010c
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-04-29 14:01:44 -0400

    Test out inset behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { BigList } from "./BigList";
 
@@ -14,6 +18,24 @@ export const App = () => {
           <BigList expanded />
         </main>
       </div>
+    </SidebarProvider>
+  );
+};
+
+// This lays out the sidebar with an inset
+export const AppWithInset = () => {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <div className="flex flex-col flex-grow">
+          <div className="p-3 bg-blue-300">17Reviews</div>
+          <main className="p-3 bg-green-300 ">
+            <SidebarTrigger />
+            <BigList expanded />
+          </main>
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
