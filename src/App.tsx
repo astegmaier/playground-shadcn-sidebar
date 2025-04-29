1bd0b43e9fb0f18333715b8116e4b939fd0441a5
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-04-29 13:19:21 -0400

    Basic sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./AppSidebar";
 import { BigList } from "./BigList";
 
 export const App = () => {
   return (
-    <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr] h-screen w-screen overflow-hidden">
-      <div className="p-3 bg-blue-300 col-span-2">17Reviews</div>
-      <div className="p-3 bg-red-300 overflow-auto max-h-full">
-        <BigList />
-      </div>
-      <div className="p-3 bg-green-300 overflow-auto max-h-full">
-        <BigList expanded />
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <div className="p-3 bg-green-300 overflow-auto max-h-full">
+          <SidebarTrigger />
+          <BigList expanded />
+        </div>
+      </main>
+    </SidebarProvider>
   );
 };
