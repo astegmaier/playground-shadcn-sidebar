9c29e06a4498ca2ef26372e4ee1b8b0f7e51c6e8
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-04-29 12:58:53 -0400

    Format the components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BigList } from "./BigList"
+import { BigList } from "./BigList";
 
 export const App = () => {
   return (
     <div className="flex-col">
-      <div className="p-3 bg-blue-300">
-        17Reviews
-      </div>
+      <div className="p-3 bg-blue-300">17Reviews</div>
       <div className="p-3 bg-red-300">Sidebar</div>
-      <div className="p-3 bg-green-300"><BigList /></div>
+      <div className="p-3 bg-green-300">
+        <BigList />
+      </div>
     </div>
-  )
-}
-
-
+  );
+};
--- src/BigList.tsx
+++ src/BigList.tsx
@@ -1,13 +1,10 @@
 export const BigList = () => {
-    const stuff = Array(1000).fill("stuff");
-    return (
-      <ul>
-        {stuff.map((item, index) => (
-          <li key={index}>
-            {item}
-          </li>
-        ))}
-      </ul>
-    )
-  }
-  
+  const stuff = Array(1000).fill("stuff");
+  return (
+    <ul>
+      {stuff.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
