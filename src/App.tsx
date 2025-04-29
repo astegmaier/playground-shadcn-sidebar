9898588499ba8aa112e3fa5e91ae47419b87a7e2
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-04-29 13:09:31 -0400

    Add expanded/collapsed.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ export const App = () => {
   return (
     <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr] h-screen w-screen overflow-hidden">
       <div className="p-3 bg-blue-300 col-span-2">17Reviews</div>
-      <div className="p-3 bg-red-300 overflow-auto max-h-full">Sidebar</div>
-      <div className="p-3 bg-green-300 overflow-auto max-h-full">
+      <div className="p-3 bg-red-300 overflow-auto max-h-full">
         <BigList />
+      </div>
+      <div className="p-3 bg-green-300 overflow-auto max-h-full">
+        <BigList expanded />
       </div>
     </div>
   );
--- src/BigList.tsx
+++ src/BigList.tsx
@@ -1,10 +1,25 @@
-export const BigList = () => {
+import { useState } from "react";
+
+interface BigListProps {
+  expanded?: boolean;
+}
+
+export const BigList = ({ expanded }: BigListProps) => {
+  const [isExpanded, setIsExpanded] = useState(!!expanded);
   const stuff = Array(1000).fill("stuff");
+
   return (
-    <ul>
-      {stuff.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
+    <>
+      <p>
+        <button type="button" onClick={() => setIsExpanded((prev) => !prev)}>
+          {isExpanded ? "Collapse" : "Expand"}
+        </button>
+      </p>
+      <ul>
+        {(isExpanded ? stuff : []).map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 };
